83b4aae074a36f804406ff44361a41ba73e6aeba
Ithyx <lamidey.m@gmail.com>
2025-04-13 00:10:12 -0400

    Add util scope timer

---
+++ src/debug.rs
@@ -0,0 +1,26 @@
+pub(crate) struct ScopeTimer {
+    name: String,
+    log_level: log::Level,
+    timer: std::time::Instant,
+}
+
+impl ScopeTimer {
+    pub fn new(log_level: log::Level, name: String) -> Self {
+        Self {
+            name,
+            log_level,
+            timer: std::time::Instant::now(),
+        }
+    }
+}
+
+impl Drop for ScopeTimer {
+    fn drop(&mut self) {
+        log::log!(
+            self.log_level,
+            "{} took {}ms",
+            self.name,
+            self.timer.elapsed().as_millis()
+        );
+    }
+}
